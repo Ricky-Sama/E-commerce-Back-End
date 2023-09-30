b224d31329d6824e2ec1722cb69fe1d156c6c3cc
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-09-30 13:34:47 -0400

    added code and comments to tag.js

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -7,6 +7,15 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, // Data type set to INTEGER
+      allowNull: false, // Reject NULL values
+      primaryKey: true, // Set as the primary key
+      autoIncrement: true, // Automatically increment the value for each new record
+    },
+    tag_name: {
+      type: DataTypes.STRING, // Data type set to STRING
+    },
   },
   {
     sequelize,
